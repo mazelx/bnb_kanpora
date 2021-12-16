aedf8a4e72c88b9870472455d2b438c218697354
Xavier Mazellier <xavier.mazellier@gmail.com>
2021-12-17 00:06:13 +0100

    [CHORE] Fix requirements

--- dataviz/dataviz.py
+++ dataviz/dataviz.py
@@ -28,6 +28,7 @@ def load_data(nrows=None):
         data = data.head(nrows)
     data['license'].fillna("", inplace=True)
     data['has_license'] = data.license.str.len() > 6
+    data['city'] = data.city.str.title().str.strip()
     return data
 
 # Create a text element and let the reader know the data is loading.
@@ -68,11 +69,28 @@ fig = px.scatter_mapbox(data,
                         lon=data.longitude,
                         color=data.has_license,
                         hover_name=data.name,
-                        hover_data=[data.room_type, data.reviews, data.accommodates, data.license],
+                        hover_data={
+                            'room_type':True, 
+                            'reviews':True, 
+                            'accommodates':True, 
+                            'license':True,
+                            'latitude':False,
+                            'longitude':False,
+                            'has_license': False
+                            },
                         zoom=12,
                         size_max=1,
+                        labels={
+                            'has_license':'Avec N° Enregistrement', 
+                            'license':'Num enregistrement',
+                            'room_type':'Type de logement',
+                            'reviews':'Nb Commentaires',
+                            'accomodates':'Capacité'
+                            },
                         height=500,
-                        color_discrete_sequence=px.colors.qualitative.Pastel
+                        opacity=0.7,
+                        color_discrete_map={True: 'darkseagreen', False:'lightcoral'},
+                        category_orders={'has_license':[True, False]}
                         )
 
 fig.update_layout(mapbox_accesstoken="pk.eyJ1IjoibWF6ZWx4IiwiYSI6ImNqOG9tODMzYzA1MnAydnBjZG5lYTR4bGwifQ.R7lZcLkJejwX4D4--1yMSA")
@@ -81,7 +99,7 @@ st.plotly_chart(fig, use_container_width=True)
 
 host_room_nb = data.groupby('host_id').size().reset_index()
 host_room_nb.columns = ['host_id', 'nb']
-multi_rooms_treshold = st.sidebar.slider('Seuil multi-logements', 1, int(host_room_nb.nb.max()), 2)
+multi_rooms_treshold = st.sidebar.slider('Seuil multi-logements', 1, int(host_room_nb.nb.max()), 10)
 multi_room_hosts = host_room_nb[host_room_nb.nb >= multi_rooms_treshold].host_id.unique()
 data_multi_rooms = data[data.host_id.isin(multi_room_hosts)].sort_values(by="host_id", ascending=True)
 
@@ -97,15 +115,32 @@ col3.metric("Loueurs", len(data_multi_rooms.host_id.unique()))
 
 # Map
 fig_multi_rooms = px.scatter_mapbox(data_multi_rooms, 
-                                    lat=data.latitude,
-                                    lon=data.longitude,
-                                    color=data.has_license,
-                                    hover_name=data.name,
-                                    hover_data=[data.room_type, data.reviews, data.accommodates, data.license],
+                                    lat=data_multi_rooms.latitude,
+                                    lon=data_multi_rooms.longitude,
+                                    color=data_multi_rooms.has_license,
+                                    hover_name=data_multi_rooms.name,
+                                    hover_data={
+                                        'room_type':True, 
+                                        'reviews':True, 
+                                        'accommodates':True, 
+                                        'license':True,
+                                        'latitude':False,
+                                        'longitude':False,
+                                        'has_license': False
+                                        },
                                     zoom=12,
                                     size_max=1,
+                                    labels={
+                                        'has_license':'Avec N° Enregistrement', 
+                                        'license':'Num enregistrement',
+                                        'room_type':'Type de logement',
+                                        'reviews':'Nb Commentaires',
+                                        'accomodates':'Capacité'
+                                        },
                                     height=500,
-                                    color_discrete_sequence=px.colors.qualitative.Pastel
+                                    opacity=0.7,
+                                    color_discrete_map={True: 'darkseagreen', False:'lightcoral'},
+                                    category_orders={'has_license':[True, False]}
                         )
 
 fig_multi_rooms.update_layout(
